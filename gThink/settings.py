7569ac2236d93b6fcdfb8413a86dc87affb7b3c4
g-think <lalith.yes@gmail.com>
2023-01-24 00:48:33 +0530

    new-database-02-2-5r

--- gThink/settings.py
+++ gThink/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'h%^rr-#khhs3j0&$7q9%u$2jymbe=0p7v^-g%=mvvnlb$0939e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['www.gthinkinventors.in', 'gthinkinventors.in', '*']  # 'allnew-env.eba-mmj3qtva.ap-south-1.elasticbeanstalk.com', 'secound.ap-south-1.elasticbeanstalk.com', '*']
 
